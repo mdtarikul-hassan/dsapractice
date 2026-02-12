1b704d8e24b431aa2283a1eabbf1a0b7bce33c8d
tarikul <mdtarikulhassan5432@gmail.com>
2026-02-12 23:57:39 +0530

    solve q27 Longest Subarray with given Sum K(Positives) by sliding window algorithm and this is the optimal one

--- LongestSubarraywithGivenSumkPositive.java
+++ LongestSubarraywithGivenSumkPositive.java
@@ -1,18 +1,42 @@
 public class LongestSubarraywithGivenSumkPositive{
     public int longestSum(int[] arr, int targetSum){
-        int maxLength = 0;
-        for(int i = 0; i< arr.length; i++){
-            for(int j = i; j< arr.length; j++){
-                int currentSum = 0;
-                for(int k= i; k<= j; k++){
-                    currentSum += arr[k];
-                }
-                if(currentSum == targetSum){
-                    maxLength = Math.max(maxLength, j-i+1);
-                }
+    // ------------------- brute force -------------
+        // int maxLength = 0;
+        // for(int i = 0; i< arr.length; i++){
+        //     for(int j = i; j< arr.length; j++){
+        //         int currentSum = 0;
+        //         for(int k= i; k<= j; k++){
+        //             currentSum += arr[k];
+        //         }
+        //         if(currentSum == targetSum){
+        //             maxLength = Math.max(maxLength, j-i+1);
+        //         }
+        //     }
+        // }
+        // return maxLength;
+
+    // -------------- optimal [sliding window] -----------------
+        int n = arr.length;
+        int maxLen = 0;
+        int left = 0, right = 0;
+
+        int sum = arr[0];
+        while (right < n) {
+            while (left <= right && sum > targetSum) {
+                sum -= arr[left];
+                left++;
+            }
+
+            if (sum == targetSum) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+            right++;
+            if (right < n) {
+                sum += arr[right];
             }
         }
-        return maxLength;
+
+        return maxLen;
     }
     public static void main(String[] args) {
         int[] arr = {10, 5, 2, 7, 1, 9};
