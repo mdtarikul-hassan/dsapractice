17bff9ceacf6e97d0d77b2826625f1feebe6a118
tarikul <mdtarikulhassan5432@gmail.com>
2026-02-13 00:36:56 +0530

    solve q27 longest subarray with given sum k (positive) by better one

--- LongestSubarraywithGivenSumkPositive.java
+++ LongestSubarraywithGivenSumkPositive.java
@@ -15,28 +15,41 @@ public class LongestSubarraywithGivenSumkPositive{
         // }
         // return maxLength;
 
-    // -------------- optimal [sliding window] -----------------
-        int n = arr.length;
-        int maxLen = 0;
-        int left = 0, right = 0;
-
-        int sum = arr[0];
-        while (right < n) {
-            while (left <= right && sum > targetSum) {
-                sum -= arr[left];
-                left++;
-            }
-
-            if (sum == targetSum) {
-                maxLen = Math.max(maxLen, right - left + 1);
-            }
-            right++;
-            if (right < n) {
-                sum += arr[right];
+    // -------------------- better one --------------------
+        int maxLength = 0;
+        for(int i = 0; i< arr.length; i++){
+            int currentSum = 0;
+            for(int j = i; j< arr.length; j++){
+                currentSum += arr[j];
+                if(currentSum == targetSum){
+                    maxLength = Math.max(maxLength, j-i+1);
+                }
             }
         }
+        return maxLength;
 
-        return maxLen;
+    // -------------- optimal [sliding window] -----------------
+        // int n = arr.length;
+        // int maxLen = 0;
+        // int left = 0, right = 0;
+
+        // int sum = arr[0];
+        // while (right < n) {
+        //     while (left <= right && sum > targetSum) {
+        //         sum -= arr[left];
+        //         left++;
+        //     }
+
+        //     if (sum == targetSum) {
+        //         maxLen = Math.max(maxLen, right - left + 1);
+        //     }
+        //     right++;
+        //     if (right < n) {
+        //         sum += arr[right];
+        //     }
+        // }
+
+        // return maxLen;
     }
     public static void main(String[] args) {
         int[] arr = {10, 5, 2, 7, 1, 9};
