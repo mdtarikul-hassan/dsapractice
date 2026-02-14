abf73ac7566d958dcf81e895dc8d51715b235c91
tarikul <mdtarikulhassan5432@gmail.com>
2026-02-14 12:32:07 +0530

    solve q29 by count subarray sum equals to k by optimal approach using hashing T.C. O(n)

--- CountSubarraySumequalsK.java
+++ CountSubarraySumequalsK.java
@@ -1,3 +1,6 @@
+
+import java.util.HashMap;
+
 public class CountSubarraySumequalsK{
     static int countSubArray(int[] arr, int k){
     // ---------------- brute force ------------------ O(n3)
@@ -17,15 +20,31 @@ public class CountSubarraySumequalsK{
         // return count;
     
     // ---------------- better approach ------------------ O(n2)
+        // int count = 0;
+        // for (int i = 0; i < arr.length; i++) {
+        //     int currSum = 0;
+        //     for (int j = i; j < arr.length; j++) {
+        //         currSum += arr[j];
+        //         if(currSum == k){
+        //             count++;
+        //         }
+        //     }
+        // }
+
+        // return count;
+    
+    // ---------------------- optimal by hashing -------------- O(n)
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
         int count = 0;
-        for (int i = 0; i < arr.length; i++) {
-            int currSum = 0;
-            for (int j = i; j < arr.length; j++) {
-                currSum += arr[j];
-                if(currSum == k){
-                    count++;
-                }
+        int sum = 0;
+        for (int j = 0; j < arr.length; j++) {
+            sum += arr[j];
+            int remove = sum -k;
+            if(map.containsKey(remove)){
+                count += map.get(remove);
             }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
 
         return count;
