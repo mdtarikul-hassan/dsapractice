9b340dac70cb6b56a51f1419c1ccd4f7e5759f11
tarikul <mdtarikulhassan5432@gmail.com>
2026-02-14 12:05:00 +0530

    solve q29 by count subarray sum equals to k by better approach using nested loop T.C. O(n2)

--- CountSubarraySumequalsK.java
+++ CountSubarraySumequalsK.java
@@ -1,13 +1,27 @@
 public class CountSubarraySumequalsK{
     static int countSubArray(int[] arr, int k){
     // ---------------- brute force ------------------ O(n3)
+        // int count = 0;
+        // for (int i = 0; i < arr.length; i++) {
+        //     for (int j = i; j < arr.length; j++) {
+        //         int currSum = 0;
+        //         for (int idx = i; idx <= j; idx++) {
+        //             currSum += arr[idx];
+        //         }
+        //         if(currSum == k){
+        //             count++;
+        //         }
+        //     }
+        // }
+
+        // return count;
+    
+    // ---------------- better approach ------------------ O(n2)
         int count = 0;
         for (int i = 0; i < arr.length; i++) {
-            for (int j = i+1; j < arr.length; j++) {
-                int currSum = 0;
-                for (int idx = i; idx <= j; idx++) {
-                    currSum += arr[idx];
-                }
+            int currSum = 0;
+            for (int j = i; j < arr.length; j++) {
+                currSum += arr[j];
                 if(currSum == k){
                     count++;
                 }
