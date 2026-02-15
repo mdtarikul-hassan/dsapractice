3f76f3ca9bf15211e5d6aa018b631859bb17a41c
tarikul <mdtarikulhassan5432@gmail.com>
2026-02-16 01:54:11 +0530

    solve q25 two sum by hashing

--- TwoSum.java
+++ TwoSum.java
@@ -1,17 +1,28 @@
+import java.util.HashMap;
+
 public class TwoSum {
     public static int[] twoSum(int[] nums, int target) {
     // ---------------------------- brute force ------------------------
-        for(int i = 0; i<nums.length; i++){
-            for(int j=i+1; j< nums.length; j++){
-                if(nums[i] + nums[j] == target){
-                    return new int[]{i,j};
-                }
-            }
-        }
-        return null;
-    // ------------------------ optimal [by using hashing or two pointer] --------------------
-        // it's complete after hashing
+        // for(int i = 0; i<nums.length; i++){
+        //     for(int j=i+1; j< nums.length; j++){
+        //         if(nums[i] + nums[j] == target){
+        //             return new int[]{i,j};
+        //         }
+        //     }
+        // }
+        // return null;
 
+    // ------------------------ optimal [by using hashing] -------------------- O(n)
+        // it's complete after hashing
+         HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] { -1, -1 };
         
     }
     public static void main(String[] args){
