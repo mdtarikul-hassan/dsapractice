fa03f8e8e733ced30dfab60ceafd01cdff9c88b1
tarikul <mdtarikulhassan5432@gmail.com>
2026-02-15 23:55:28 +0530

    solve q14 Second Largest Element in an array by better approach, T. C. = O(n)

--- SecondLargestElement.java
+++ SecondLargestElement.java
@@ -1,18 +1,39 @@
-import java.util.Arrays;
 
 public class SecondLargestElement {
     static void getElements(int[] arr, int n) {
+    // -------------------- brute force ------------- O(nlong)
+        // if (n == 0 || n == 1) {
+        //     System.out.println(-1 + " " + -1);  
+        //     return;
+        // }
+        // Arrays.sort(arr);
+        // int small = arr[1];
+        // int large = arr[n - 2];
+
+        // System.out.println("Second smallest is " + small);
+        // System.out.println("Second largest is " + large);
+
+    // ---------------------- better approach -------------- O(n)
         if (n == 0 || n == 1) {
-            System.out.println(-1 + " " + -1);  
+            System.out.println(-1 + " " + -1);
             return;
         }
-
-        Arrays.sort(arr);
-        int small = arr[1];
-        int large = arr[n - 2];
-
-        System.out.println("Second smallest is " + small);
-        System.out.println("Second largest is " + large);
+        int small = Integer.MAX_VALUE, second_small = Integer.MAX_VALUE;
+        int large = Integer.MIN_VALUE, second_large = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            small = Math.min(small, arr[i]);
+            large = Math.max(large, arr[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < second_small && arr[i] != small) {
+                second_small = arr[i];
+            }
+            if (arr[i] > second_large && arr[i] != large) {
+                second_large = arr[i];
+            }
+        }
+        System.out.println("Second smallest is " + second_small);
+        System.out.println("Second largest is " + second_large);
     }
 
     public static void main(String[] args) {
