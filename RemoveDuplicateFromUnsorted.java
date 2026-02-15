0d4be34c0676fe6ac69c3b60108919833e860bac
tarikul <mdtarikulhassan5432@gmail.com>
2026-02-16 01:49:59 +0530

    solve q26 remove duplicate from unsorted array by hashing optimal solution, TC = O(n)

--- RemoveDuplicateFromUnsorted.java
+++ RemoveDuplicateFromUnsorted.java
@@ -1,27 +1,35 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class RemoveDuplicateFromUnsorted {
     public static ArrayList<Integer> removeDupliacte(int[] nums) {
-    // -----------brute force -----------------------
-        ArrayList<Integer> arrList = new ArrayList<>();
-        for(int i = 0; i< nums.length; i++){
-            boolean found = false;
-            for(int j = 0; j< arrList.size(); j++){
-                if(nums[i] == arrList.get(j)){
-                    found = true;
-                    break;
-                }
-            }
-            if(!found){
-                arrList.add(nums[i]);
+    // -----------brute force ----------------------- O(n2)
+        // ArrayList<Integer> arrList = new ArrayList<>();
+        // for(int i = 0; i< nums.length; i++){
+        //     boolean found = false;
+        //     for(int j = 0; j< arrList.size(); j++){
+        //         if(nums[i] == arrList.get(j)){
+        //             found = true;
+        //             break;
+        //         }
+        //     }
+        //     if(!found){
+        //         arrList.add(nums[i]);
+        //     }
+        // }
+        // return arrList;
+
+    // ------------------------ optimal [by hashing] ---------------- O(n)
+        HashMap<Integer, Boolean> seen = new HashMap<>();
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int val : nums) {
+            if (!seen.containsKey(val)) {
+                result.add(val);
+                seen.put(val, true);
             }
         }
-        
-        return arrList;
-
-    // ------------------------ optimal [by hashing] ----------------
-
-
+        return result;
     
     }
 
