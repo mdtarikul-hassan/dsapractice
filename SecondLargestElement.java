244d000435b8f61caa6204451884b664116d1a33
tarikul <mdtarikulhassan5432@gmail.com>
2026-02-15 23:48:31 +0530

    solve q14 Second Largest Element in an array by brute force approach, T. C. = O(nlogn)

---
+++ SecondLargestElement.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class SecondLargestElement {
+    static void getElements(int[] arr, int n) {
+        if (n == 0 || n == 1) {
+            System.out.println(-1 + " " + -1);  
+            return;
+        }
+
+        Arrays.sort(arr);
+        int small = arr[1];
+        int large = arr[n - 2];
+
+        System.out.println("Second smallest is " + small);
+        System.out.println("Second largest is " + large);
+    }
+
+    public static void main(String[] args) {
+        
+        int[] arr = {1, 2, 4, 6, 7, 5};
+        int n = arr.length;
+        getElements(arr, n);
+    }
+}
