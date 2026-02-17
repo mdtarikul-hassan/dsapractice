80af86de31e9ab34a7ee75b400684824cdbc8d15
tarikul <mdtarikulhassan5432@gmail.com>
2026-02-17 23:34:00 +0530

    solve q31 Find the Majority Element thet occurs N/2 times by using loop of an array TC:O(n)

--- MajorityElementnby2.java
+++ MajorityElementnby2.java
@@ -1,5 +1,3 @@
-import java.util.HashMap;
-import java.util.Set;
 
 public class MajorityElementnby2 {
     public static int Majority(int[] nums) {
@@ -17,25 +15,46 @@ public class MajorityElementnby2 {
         // }
         // return -1;
         
-    // -------------------- better by Hashmap ---------------
-        int n = nums.length;
-        int major = -1;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i< n; i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i], map.get(nums[i]) + 1);
-            }else{
-                map.put(nums[i], 1);
-            }
-        }
+    // -------------------- better by Hashmap --------------- O(n)
+        // int n = nums.length;
+        // int major = -1;
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for(int i=0; i< n; i++){
+        //     // if(map.containsKey(nums[i])){
+        //     //     map.put(nums[i], map.get(nums[i]) + 1);
+        //     // }else{
+        //     //     map.put(nums[i], 1);
+        //     // }
+        //     map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        // }
 
-        Set<Integer> keySet = map.keySet();
-        for(Integer key : keySet){
-            if(map.get(key) > n/2){
-                major = key;
+        // // Set<Integer> keySet = map.keySet();
+        // // for(Integer key : keySet){
+        // //     if(map.get(key) > n/2){
+        // //         major = key;
+        // //     }
+        // // }
+        // for(Integer key : map.keySet()){
+        //     if(map.get(key) > n/2){
+        //         major = key;
+        //     }
+        // }
+        // return major;
+    
+    // -------------- better or optimal by array ------------
+        int n = nums.length;
+        int i = 0;
+        int count = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if(nums[j] == nums[i]){
+                count++;
+            }if(count > (n/2)){
+                return nums[j];
             }
+            i++;
         }
-        return major;
+        
+        return -1;
     }
         
     public static void main(String[] args){
