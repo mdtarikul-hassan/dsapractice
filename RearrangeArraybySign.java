5b8d3d440c61b5bad29fbe4e19eccbb7b3586391
tarikul <mdtarikulhassan5432@gmail.com>
2026-02-20 00:49:07 +0530

    solve q35 Rearrange Array Elements by Sign for same no of positive and negetive number by optimar approach; TC : O(n)

--- RearrangeArraybySign.java
+++ RearrangeArraybySign.java
@@ -1,28 +1,38 @@
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class RearrangeArraybySign {
-// ------------------ brute force ------------------- O(n+n/2) = O(n)
     static int[] RearrangeArray(int[] arr){
-        List<Integer> pos = new ArrayList<>();
-        List<Integer> neg = new ArrayList<>();
+    // ------------------ brute force ------------------- O(n+n/2) = O(n)
+        // List<Integer> pos = new ArrayList<>();
+        // List<Integer> neg = new ArrayList<>();
 
+        // for(int i = 0; i< arr.length; i++){
+        //     if(arr[i] > 0){
+        //         pos.add(arr[i]);
+        //     }else{
+        //         neg.add(arr[i]);
+        //     }
+        // }
+
+        // for(int i = 0; i< pos.size(); i++){ // for pos.size() == neg.size()
+        //     arr[2*i] = pos.get(i);
+        //     arr[2*i + 1] = neg.get(i);
+        // }
+        // return arr;
+    
+    // ---------------------- optimal ---------------- O(n)
+        int[] ans = new int[arr.length];
+        int posIdx = 0, negIdx = 1;
         for(int i = 0; i< arr.length; i++){
             if(arr[i] > 0){
-                pos.add(arr[i]);
+                ans[posIdx] = arr[i];
+                posIdx = posIdx+2;
             }else{
-                neg.add(arr[i]);
+               ans[negIdx] = arr[i];
+                negIdx = negIdx+2;
             }
         }
-
-        for(int i = 0; i< pos.size(); i++){ // for pos.size() == neg.size()
-            arr[2*i] = pos.get(i);
-            arr[2*i + 1] = neg.get(i);
-        }
-
-
-        return arr;
+        return ans;
     }
     public static void main(String[] args) {
         int[] A = {1,2,-3,-1,-2,3};
