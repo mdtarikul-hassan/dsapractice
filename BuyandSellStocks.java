0c2e508f3f82a5f352acee2a0158031191d932a3
tarikul <mdtarikulhassan5432@gmail.com>
2026-02-20 00:05:29 +0530

    solve q34 Best time to buy and sell stock by optimal approach; TC : O(n)

--- BuyandSellStocks.java
+++ BuyandSellStocks.java
@@ -1,14 +1,26 @@
 public class BuyandSellStocks {
     static int buyandSell(int[] arr){
     // ----------------- brute force approach --------- O(n2)
+        // int maxProfit = 0;
+        // for(int i = 0; i< arr.length; i++){
+        //     int profit = 0;
+        //     for(int j = i+1; j< arr.length; j++){
+        //         profit = arr[j] - arr[i];
+        //         maxProfit = Math.max(maxProfit, profit);
+        //     }
+        // }
+        // return maxProfit;
+
+    // ---------------- optimal --------------- O(n)
         int maxProfit = 0;
+        int min = arr[0];
         for(int i = 0; i< arr.length; i++){
-            int profit = 0;
-            for(int j = i+1; j< arr.length; j++){
-                profit = arr[j] - arr[i];
-                maxProfit = Math.max(maxProfit, profit);
+            if(arr[i] < min){
+                min = arr[i];
             }
+            maxProfit = Math.max(maxProfit, arr[i] - min);
         }
+   
         return maxProfit;
     }
     public static void main(String[] args){
