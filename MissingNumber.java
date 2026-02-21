429ba3a7764bfe07131360ef3f8b013988046fc5
tarikul <mdtarikulhassan5432@gmail.com>
2026-02-21 14:49:22 +0530

    solve q43 Find the Missing Number by optimal approach using sum of n numbers formula; TC = O(n)

--- MissingNumber.java
+++ MissingNumber.java
@@ -1,19 +1,29 @@
 public class MissingNumber {
     static int missing(int[] arr){
     // ---------------------- brute force ----------- O(n2)
-        for(int i = 1; i< arr.length+1; i++){
-            boolean found  =false;
-            for (int j = 0; j < arr.length; j++) {
-                if(arr[j] == i){
-                    found = true;
-                    break;
-                }
-            }
-            if(!found){
-                return i;
-            }
+        // for(int i = 1; i< arr.length+1; i++){
+        //     boolean found  =false;
+        //     for (int j = 0; j < arr.length; j++) {
+        //         if(arr[j] == i){
+        //             found = true;
+        //             break;
+        //         }
+        //     }
+        //     if(!found){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+    // ----------------------- optimal by sum of n numbers approach ------------------ O(n)
+        int n=arr.length +1;
+        int sum = 0;
+        for (int i = 0; i < n-1; i++) {
+            sum += arr[i];
         }
-        return -1;
+        int expSum = (n*(n+1)) / 2;
+        int missing = expSum - sum;
+        return missing;
     }
     public static void main(String[] args) {
         int[] arr = {8, 2, 4, 5, 3, 7, 1};
