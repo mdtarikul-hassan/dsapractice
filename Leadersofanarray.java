e908bb622d2e0a682fc41b86d5632ec29d274968
tarikul <mdtarikulhassan5432@gmail.com>
2026-02-21 15:55:47 +0530

    solve q37 Leaders in an Array by optimal way Using ArrayList ; TC = O(n)

--- Leadersofanarray.java
+++ Leadersofanarray.java
@@ -3,19 +3,38 @@ import java.util.*;
 public class Leadersofanarray {
     static List<Integer> leader(int[] arr){
     // -------------- brute force -------------  O(n2)
-        List<Integer> ans = new ArrayList<>();
-        for(int i = 0; i< arr.length; i++){
-            boolean leader  =true;
-            for (int j = i+1; j < arr.length; j++) {
-                if(arr[j] >= arr[i]){
-                    leader = false;
-                    break;
-                }
-            }
-            if(leader){
-               ans.add(arr[i]); 
+        // List<Integer> ans = new ArrayList<>();
+        // for(int i = 0; i< arr.length; i++){
+        //     boolean leader  =true;
+        //     for (int j = i+1; j < arr.length; j++) {
+        //         if(arr[j] >= arr[i]){
+        //             leader = false;
+        //             break;
+        //         }
+        //     }
+        //     if(leader){
+        //        ans.add(arr[i]); 
+        //     }
+        // }
+        // return ans;
+    
+    // ----------------- optimal -------------------- O(n)
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        if(arr.length == 0){
+            return ans;
+        }
+        int max = arr[arr.length -1];
+        ans.add(arr[arr.length -1]);
+        for(int i = arr.length -2; i> 0; i--){
+            if(arr[i] > max){
+                max = arr[i];
+                ans.add(arr[i]);
             }
         }
+
+        Collections.reverse(ans);
+
         return ans;
     }
     public static void main(String[] args) {
