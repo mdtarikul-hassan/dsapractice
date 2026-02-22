4b8b809f6f4bbda1f87e78c49ed7a42b8c57731c
tarikul <mdtarikulhassan5432@gmail.com>
2026-02-22 16:01:03 +0530

    solve q41 Union of Two Sorted Arrays by better approach using set; TC = O((n + m) * (log (n + m)))

--- UnionoftwoSortedArray.java
+++ UnionoftwoSortedArray.java
@@ -1,27 +1,41 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
 
 public class UnionoftwoSortedArray {
-    static ArrayList<Integer> findUnion(int[] a, int[] b){
     // -------------------- brute force approach ------------- o(n*m)
-        ArrayList<Integer> res = new ArrayList<>();
-        for (int i = 0; i < a.length; i++) {
-            if (!res.contains(a[i])) {
-                res.add(a[i]);
-            }
+
+    // static ArrayList<Integer> findUnion(int[] a, int[] b){
+        // ArrayList<Integer> res = new ArrayList<>();
+        // for (int i = 0; i < a.length; i++) {
+        //     if (!res.contains(a[i])) {
+        //         res.add(a[i]);
+        //     }
+        // }
+        // for (int i = 0; i < b.length; i++) {
+        //     if (!res.contains(b[i])) {
+        //         res.add(b[i]);
+        //     }
+        // }
+        // Collections.sort(res);
+        // return res;
+    // }
+
+    // ---------------------- better by using set ----------------- O((n + m) * (log (n + m)))
+    static Set<Integer> findUnion(int[] a, int[] b){
+        Set<Integer> res = new TreeSet<>();
+        for (int num : a) {
+            res.add(num);
         }
-        for (int i = 0; i < b.length; i++) {
-            if (!res.contains(b[i])) {
-                res.add(b[i]);
-            }
+        for (int num : b) {
+            res.add(num);
         }
-        Collections.sort(res);
         return res;
     }
     public static void main(String[] args) {
         int[] a = {1, 1, 2, 2, 2, 4};
         int[] b = {2, 2, 4, 4};
-        ArrayList<Integer> newArr = findUnion(a, b);
+
+        // ArrayList<Integer> newArr = findUnion(a, b);
+        Set<Integer> newArr = findUnion(a, b);
 
         for(int num : newArr) {
             System.out.print(num + " ");
