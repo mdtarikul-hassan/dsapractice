f8ce7774846c6064c55325247754562754c0d81e
tarikul <mdtarikulhassan5432@gmail.com>
2026-03-04 00:38:13 +0530

    solve q63 Count frequency of each element in the array by better way using binary search approach; TC = O(nlogn)

--- FrequencyofElements.java
+++ FrequencyofElements.java
@@ -1,29 +1,50 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class FrequencyofElements {
     static ArrayList<ArrayList<Integer>> countFrequency(int[] arr){
     // --------------------- brute force approach ---------------- O(n2)
-        int n = arr.length;
-        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
-        boolean[] visited = new boolean[n];
+        // int n = arr.length;
+        // ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        // boolean[] visited = new boolean[n];
 
-        for(int i = 0; i< n; i++){
-            if(visited[i]){
-                continue;
+        // for(int i = 0; i< n; i++){
+        //     if(visited[i]){
+        //         continue;
+        //     }
+        //     int count = 1;
+        //     for(int j= i+1; j< n; j++){
+        //         if(arr[i] == arr[j]){
+        //             visited[j]= true;
+        //             count++;
+        //         }
+        //     }
+        //     ArrayList<Integer> temp = new ArrayList<>();
+        //     temp.add(arr[i]);
+        //     temp.add(count);
+        //     ans.add(temp);
+        // }
+        // return ans;
+
+    // --------------------- better by binary search ---------------- O(nlogn)
+        Arrays.sort(arr);
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        int n = arr.length;
+        int i = 0;
+        while(i<n){
+            int current = arr[i];
+            int firstIdx = i;
+            int lastIdx = i;
+            while(lastIdx + 1 <n && arr[lastIdx+1] == current){
+                lastIdx++;
             }
-            int count = 1;
-            for(int j= i+1; j< n; j++){
-                if(arr[i] == arr[j]){
-                    visited[j]= true;
-                    count++;
-                }
-            }
+            int count = lastIdx - firstIdx +1;
             ArrayList<Integer> temp = new ArrayList<>();
-            temp.add(arr[i]);
+            temp.add(current);
             temp.add(count);
             ans.add(temp);
+            i = lastIdx + 1;
         }
-
         return ans;
     }
     public static void main(String[] args) {
