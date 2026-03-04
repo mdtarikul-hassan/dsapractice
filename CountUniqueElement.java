92158f3283faea8458943ef8bd2298b1050ee7d1
tarikul <mdtarikulhassan5432@gmail.com>
2026-03-04 14:57:56 +0530

    solve q65 count distinct / unique element in an array by better approach using sort; TC = O(nlogn)

--- CountUniqueElement.java
+++ CountUniqueElement.java
@@ -1,18 +1,32 @@
+import java.util.*;
+
 public class CountUniqueElement {
     static int countDistinct(int[] arr){
     // ----------------- brute force approach ---------------- O(n2)
+        // int n= arr.length;
+        // int count = 1;
+        // for(int i = 1; i< n; i++){
+        //     int j;
+        //     for(j = 0; j< i; j++){
+        //         if(arr[i] == arr[j]){
+        //             break;
+        //         }
+        //     }
+        //     if(i == j){
+        //         count++;
+        //     }
+        // }
+        // return count;
+
+    // ----------------- better approach by array sort -------------- O(nlogn)
         int n= arr.length;
+        Arrays.sort(arr);
         int count = 1;
-        for(int i = 1; i< n; i++){
-            int j;
-            for(j = 0; j< i; j++){
-                if(arr[i] == arr[j]){
-                    break;
-                }
+        for(int i = 0; i< n; i++){
+            while(i < n-1 && arr[i] == arr[i+1]){
+                i++;
             }
-            if(i == j){
-                count++;
-            }
+            count++;
         }
         return count;
     }
