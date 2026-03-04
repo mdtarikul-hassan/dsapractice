dbe6acbb782e05e13d150e4039dc491b82fd3260
tarikul <mdtarikulhassan5432@gmail.com>
2026-03-04 16:27:52 +0530

    solve q71 Find first non-repeating element in a given Array of integers by optimal approach using hashing; TC = o(n)

--- FirstNonRepeatingElement.java
+++ FirstNonRepeatingElement.java
@@ -1,15 +1,34 @@
+import java.util.HashMap;
+
 public class FirstNonRepeatingElement {
-// ------------------------ brute force approach ------------ O(n2)
     static int firstNonRepeating(int[] arr){
+    // ------------------------ brute force approach ------------ O(n2)
+        // int n = arr.length;
+        // for(int i = 0; i< n; i++){
+        //     int j;
+        //     for(j = 0; j< n; j++){
+        //         if(i != j && arr[i] == arr[j]){
+        //             break;
+        //         }
+        //     }
+        //     if(j == n){
+        //         return arr[i];
+        //     }
+        // }
+        // return -1;
+
+    // ------------------- optimal by hashing ---------------- O(n)
         int n = arr.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i< n; i++){
-            int j;
-            for(j = 0; j< n; j++){
-                if(i != j && arr[i] == arr[j]){
-                    break;
-                }
+            if(map.containsKey(arr[i])){
+                map.put(arr[i], map.get(arr[i]) + 1);
+            }else{
+                map.put(arr[i], 1);
             }
-            if(j == n){
+        }
+        for(int i = 0; i< n; i++){
+            if(map.get(arr[i]) == 1){
                 return arr[i];
             }
         }
