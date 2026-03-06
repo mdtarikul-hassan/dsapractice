98ba15668e7193c340870728285d85721aefc052
tarikul <mdtarikulhassan5432@gmail.com>
2026-03-06 15:39:36 +0530

    solve q70 Longest Consecutive Sequence in an Array by optimal approach using HashSet; TC = O(n)

--- LongestConsecutiveSequence.java
+++ LongestConsecutiveSequence.java
@@ -1,5 +1,5 @@
 
-import java.util.Arrays;
+import java.util.HashSet;
 
 public class LongestConsecutiveSequence {
 // ------------------- brute force approach ----------- O(n2)
@@ -31,21 +31,44 @@ public class LongestConsecutiveSequence {
 
 
 // ------------------- better approach by array.sort ----------- O(nlogn)
-    static int longestSequencebetter(int[] arr){
+    // static int longestSequencebetter(int[] arr){
+    //     int n = arr.length;
+    //     Arrays.sort(arr);
+    //     int lastSmaller = Integer.MIN_VALUE;
+    //     int longest = 1;
+    //     int count = 0;
+    //     for(int i = 0; i< n; i++){
+    //         if(arr[i]-1 == lastSmaller){
+    //             count++;
+    //             lastSmaller = arr[i];
+    //         }else if(arr[i] != lastSmaller){
+    //             count = 1;
+    //             lastSmaller = arr[i];
+    //         }
+    //         longest = Math.max(longest, count);
+    //     }
+    //     return longest;
+    // }
+
+
+// ------------------- optimal approach by hashset ----------- O(n)
+    static int longestSequenceoptimal(int[] arr){
         int n = arr.length;
-        Arrays.sort(arr);
-        int lastSmaller = Integer.MIN_VALUE;
+        HashSet<Integer> set = new HashSet<>();
         int longest = 1;
-        int count = 0;
-        for(int i = 0; i< n; i++){
-            if(arr[i]-1 == lastSmaller){
-                count++;
-                lastSmaller = arr[i];
-            }else if(arr[i] != lastSmaller){
-                count = 1;
-                lastSmaller = arr[i];
+        for(int i : arr){
+            set.add(i);
+        }
+        for(int it : set){
+            if(!set.contains(it -1)){
+                int count = 1;
+                int x = it;
+                while(set.contains(x+1)){
+                    x++;
+                    count++;
+                }
+                longest = Math.max(longest, count);
             }
-            longest = Math.max(longest, count);
         }
         return longest;
     }
@@ -54,7 +77,8 @@ public class LongestConsecutiveSequence {
         int[] a = {100, 4, 200, 1, 3, 2};
 
         // int longest = longestSequence(a);
-        int longest = longestSequencebetter(a);
+        // int longest = longestSequencebetter(a);
+        int longest = longestSequenceoptimal(a);
         System.out.println(longest);
     }
 }
