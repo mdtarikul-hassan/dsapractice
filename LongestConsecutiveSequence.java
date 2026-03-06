05f2d6c220e3fe9ef74d4bc7649966b422a97a1c
tarikul <mdtarikulhassan5432@gmail.com>
2026-03-06 15:19:35 +0530

    solve q70 Longest Consecutive Sequence in an Array by better approach using Array.Sort(); TC = O(nlogn)

--- LongestConsecutiveSequence.java
+++ LongestConsecutiveSequence.java
@@ -1,34 +1,60 @@
+
+import java.util.Arrays;
+
 public class LongestConsecutiveSequence {
-// ------------------- better approach ----------- O(n2)
-    static boolean linearSearch1(int[] arr, int x){
-        for(int i = 0; i< arr.length; i++){
-            if(arr[i] == x){
-                return true;
-            }
-        }
-        return false;
-    }
-    static int longestSequence(int[] arr){
+// ------------------- brute force approach ----------- O(n2)
+    // static boolean linearSearch1(int[] arr, int x){
+    //     for(int i = 0; i< arr.length; i++){
+    //         if(arr[i] == x){
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+    // static int longestSequence(int[] arr){
+    //     int n = arr.length;
+    //     if(n == 0){
+    //         return 0;
+    //     }
+    //     int longest = 1;
+    //     for(int i = 0; i< n; i++){
+    //         int count = 1;
+    //         int x = arr[i];
+    //         while (linearSearch1(arr, x+1) == true) { 
+    //             x++;
+    //             count++;
+    //         }
+    //         longest = Math.max(longest, count);
+    //     }
+    //     return longest;
+    // }
+
+
+// ------------------- better approach by array.sort ----------- O(nlogn)
+    static int longestSequencebetter(int[] arr){
         int n = arr.length;
-        if(n == 0){
-            return 0;
-        }
+        Arrays.sort(arr);
+        int lastSmaller = Integer.MIN_VALUE;
         int longest = 1;
+        int count = 0;
         for(int i = 0; i< n; i++){
-            int count = 1;
-            int x = arr[i];
-            while (linearSearch1(arr, x+1) == true) { 
-                x++;
+            if(arr[i]-1 == lastSmaller){
                 count++;
+                lastSmaller = arr[i];
+            }else if(arr[i] != lastSmaller){
+                count = 1;
+                lastSmaller = arr[i];
             }
             longest = Math.max(longest, count);
         }
         return longest;
     }
+
     public static void main(String[] args) {
         int[] a = {100, 4, 200, 1, 3, 2};
 
-        int longest = longestSequence(a);
+        // int longest = longestSequence(a);
+        int longest = longestSequencebetter(a);
         System.out.println(longest);
     }
 }
