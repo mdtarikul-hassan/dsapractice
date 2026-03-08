eb0d98e075c376b9374bd4ed52bafca45d41a136
tarikul <mdtarikulhassan5432@gmail.com>
2026-03-08 23:52:20 +0530

    solve q85 Check if two Strings are anagrams of each other by optimal approach; TC = O(n)

--- ValidAnagraminString.java
+++ ValidAnagraminString.java
@@ -1,23 +1,46 @@
 
-import java.util.Arrays;
 
 public class ValidAnagraminString {
     static boolean checkAnagram(String str1, String str2){
     // ----------------- brute force approach -------------- O(nlogn)
+        // if(str1.length() != str2.length()){
+        //     return false;
+        // }
+        // char[] char1 = str1.toCharArray();
+        // char[] char2 = str2.toCharArray();
+
+        // Arrays.sort(char1);
+        // Arrays.sort(char2);
+
+        // for(int i = 0; i< str1.length(); i++){
+        //     if(char1[i] != char2[i]){
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+
+    // -------------------- optimal approach ------------------ O(n)
         if(str1.length() != str2.length()){
             return false;
         }
-        char[] char1 = str1.toCharArray();
-        char[] char2 = str2.toCharArray();
 
-        Arrays.sort(char1);
-        Arrays.sort(char2);
+        int[] freq = new int[26];
 
         for(int i = 0; i< str1.length(); i++){
-            if(char1[i] != char2[i]){
+            freq[str1.charAt(i) - 'A']++;
+        }
+
+        for(int i = 0; i< str2.length(); i++){
+            freq[str2.charAt(i) - 'A']--;
+        }
+
+        for(int i=0; i<26; i++){
+            if(freq[i] != 0){
                 return false;
             }
         }
+
         return true;
     }
     public static void main(String[] args) {
