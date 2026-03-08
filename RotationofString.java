799013e9b2668bfef87733af14abe29f721aca8a
tarikul <mdtarikulhassan5432@gmail.com>
2026-03-09 03:47:54 +0530

    solve q86 Check if one string is rotation of another by optimal approach using string.contains(); TC = O(n)

--- RotationofString.java
+++ RotationofString.java
@@ -1,17 +1,24 @@
 public class RotationofString {
     static boolean rotation(String s, String goal){
-    // --------------- brute force approach ---------------- O(n)
-        if(s.length() != goal.length()){
+    // --------------- brute force approach ---------------- O(n2)
+        // if(s.length() != goal.length()){
+        //     return false;
+        // }
+        // for(int i = 0;i< s.length(); i++){
+        //     String rotated = s.substring(i) + s.substring(0,i);
+        //     if(rotated.equals(goal)){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+
+    // --------------- optimal by contains ----------------- O(n)
+       if(s.length() != goal.length()){
             return false;
         }
-        for(int i = 0;i< s.length(); i++){
-            String rotated = s.substring(i) + s.substring(0,i);
-            if(rotated.equals(goal)){
-                return true;
-            }
-        }
-
-        return false;
+        String doubleS = s+s;
+        return doubleS.contains(goal);
     }
     public static void main(String[] args) {
         String s = "rotation";
