26d623c86324dfd22e9b011fa15e05d658b30721
tarikul <mdtarikulhassan5432@gmail.com>
2026-03-19 23:10:41 +0530

    solve q15 check if an array is sorted or not : TC = O(n2)

---
+++ CheckSortedArray.java
@@ -0,0 +1,28 @@
+public class CheckSortedArray {
+    public boolean isSorted(int[] arr) {
+    // --------------------- brute force ---------- O(n2)
+        int n= arr.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[i]) 
+                    return false;
+            }
+        }
+        return true;
+
+
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        CheckSortedArray obj = new CheckSortedArray();
+        
+        boolean ans = obj.isSorted(arr);
+        
+        if (ans) 
+            System.out.println("True");
+        else 
+            System.out.println("False");
+    }
+}
