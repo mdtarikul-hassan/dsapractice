6c27b83ab005a8f7e1955ef49e6cab1816423efb
tarikul <mdtarikulhassan5432@gmail.com>
2026-03-22 23:02:40 +0530

    practice q43 Find the Missing Number of an consecutive array by sum of n numbers

--- Practices.java
+++ Practices.java
@@ -1,25 +1,34 @@
 public class Practices {
     static int missing(int[] arr){
     // --------------- btutr force ------------
-        for(int i = 1; i< arr.length+1; i++){
-            boolean found  =false;
-            for (int j = 0; j < arr.length; j++) {
-                if(arr[j] == i){
-                    found = true;
-                    break;
-                }
-            }
-            if(!found){
-                return i;
-            }
-        }
-        return -1;
+        // for(int i = 1; i< arr.length+1; i++){
+        //     boolean found  =false;
+        //     for (int j = 0; j < arr.length; j++) {
+        //         if(arr[j] == i){
+        //             found = true;
+        //             break;
+        //         }
+        //     }
+        //     if(!found){
+        //         return i;
+        //     }
+        // }
+        // return -1;
 
+    // ---------------- by sum of n number ------------
+        int n=arr.length +1;
+        int sum = 0;
+        for (int i = 0; i < n-1; i++) {
+            sum += arr[i];
+        }
+        int expSum = (n*(n+1)) / 2;
+        int missing = expSum - sum;
+        return missing;
 
     }
     public static void main(String[] args) {
 
-        // q43 find missing number in an consequetive array
+        // q43 find missing number in an consecuetive array
         int[] arr = {8, 2, 4, 5, 3, 7, 1};
         System.out.println(missing(arr));
     }
