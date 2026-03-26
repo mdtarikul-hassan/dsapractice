2f202caa0765cc7f9b9c75157b1dce68c2e327da
tarikul <mdtarikulhassan5432@gmail.com>
2026-03-26 23:53:08 +0530

    solve q124 Sum of first N Natural Numbers by recursive approach

--- SumofArrayRecur.java
+++ SumofArrayRecur.java
@@ -1,11 +1,18 @@
 public class SumofArrayRecur {
     public int sumofArray(int N){
     // ---------------------- brute force approach ----------------- O(n)
-        int sum = 0;
-        for(int i = 0; i<N; i++){
-            sum = sum +i;
+        // int sum = 0;
+        // for(int i = 1; i<=N; i++){
+        //     sum = sum +i;
+        // }
+        // return sum;
+
+    
+    // -------------------- recursive approach ------------ 
+        if(N == 1){
+            return 1;
         }
-        return sum;
+        return N+sumofArray(N -1);
     }
     public static void main(String[] args) {
         int N = 5;
