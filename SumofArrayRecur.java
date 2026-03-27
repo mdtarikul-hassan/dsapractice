0d078ca62bdd056756502d0a9758d9668b5bec29
tarikul <mdtarikulhassan5432@gmail.com>
2026-03-27 15:55:03 +0530

    solve 124 sum of all elements of an array by brute force approach; TC = O(n)

--- SumofArrayRecur.java
+++ SumofArrayRecur.java
@@ -1,5 +1,5 @@
 public class SumofArrayRecur {
-    public int sumofArray(int N){
+    // public int sumofArray(int N){
     // ---------------------- brute force approach ----------------- O(n)
         // int sum = 0;
         // for(int i = 1; i<=N; i++){
@@ -9,14 +9,24 @@ public class SumofArrayRecur {
 
     
     // -------------------- recursive approach ------------ 
-        if(N == 1){
-            return 1;
+        // if(N == 1){
+        //     return 1;
+        // }
+        // return N+sumofArray(N -1);
+    // }
+
+    public int sumofArray(int[] arr){
+    //  ----------------- brute force approach [sum of an array]-------------- O(n)
+        int sum = 0;
+        for(int i = 0; i<arr.length; i++){
+            sum = sum +arr[i];
         }
-        return N+sumofArray(N -1);
+        return sum;
     }
     public static void main(String[] args) {
+        int[] arr = {2,4,3,6,8,5,89};
         int N = 5;
         SumofArrayRecur obj = new SumofArrayRecur();
-        System.out.println(obj.sumofArray(N));
+        System.out.println(obj.sumofArray(arr));
     }
 }
