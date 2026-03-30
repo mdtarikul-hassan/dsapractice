0c58694fa7a5923fa7062bb241c49edff6bcb397
tarikul <mdtarikulhassan5432@gmail.com>
2026-03-30 23:45:58 +0530

    solve q129 Generate permutation using recursion

---
+++ GeneratePermutation.java
@@ -0,0 +1,21 @@
+public class GeneratePermutation {
+    static void printPermutn(String str, String ans){
+    // ---------------- by recursive ------------ O(n2)
+        if (str.length() == 0) {
+            System.out.print(ans + " ");
+            return;
+        }
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            String ros = str.substring(0, i) + str.substring(i + 1);
+
+            printPermutn(ros, ans + ch);
+        }
+    }
+    public static void main(String[] args){
+        String s = "abb";
+        printPermutn(s, "");
+    }
+}
