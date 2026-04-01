8044471efd1ad65ddfd1c7ac9628c5569a7e175b
tarikul <mdtarikulhassan5432@gmail.com>
2026-04-01 23:57:46 +0530

    solve q105 Last Occurrence in an array by optimal approach; TC = O(logn)

--- FirstAndLastOccurrence.java
+++ FirstAndLastOccurrence.java
@@ -4,11 +4,31 @@ import java.util.List;
 public class FirstAndLastOccurrence {
     static int solve(int n, int key, List<Integer> v) {
     // ----------------------- brute force approach (Last ocurrence) -------------- O(logn)
+        // int res = -1;
+        // for (int i = n - 1; i >= 0; i--) {
+        //     if (v.get(i) == key) {
+        //         res = i;
+        //         break;
+        //     }
+        // }
+        // return res;
+
+
+    // -------------------- optimal (Last Occurrence) ---------------- O(logn)
+        int start = 0;
+        int end = n - 1;
         int res = -1;
-        for (int i = n - 1; i >= 0; i--) {
-            if (v.get(i) == key) {
-                res = i;
-                break;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (v.get(mid) == key) {
+                res = mid;
+                start = mid + 1;
+            }
+            else if (key < v.get(mid)) {
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
             }
         }
         return res;
