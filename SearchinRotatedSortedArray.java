6a35a0bd78633302188780445f31aa15dae8df46
tarikul <mdtarikulhassan5432@gmail.com>
2026-04-02 23:49:16 +0530

    solve q106 Search Element in a Rotated Sorted Array present or not by optimal approach using INTUITION; TC = O(logn)

--- SearchinRotatedSortedArray.java
+++ SearchinRotatedSortedArray.java
@@ -35,10 +35,38 @@ public class SearchinRotatedSortedArray {
     }
 
 
-    // ------------ brute force key is present on not -------------- O(n)
     static boolean searchInARotatedSortedArrayII(int[] arr, int k) {
-        for (int num : arr) {
-            if (num == k) return true;
+    // ------------ brute force key is present on not -------------- O(n)
+        // for (int num : arr) {
+        //     if (num == k) return true;
+        // }
+        // return false;
+
+
+    // ----------------- optimal by INTUITION ------------ O(logn)
+        int low = 0, high = arr.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == k) return true;
+            if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
+                low++;
+                high--;
+                continue;
+            }
+            if (arr[low] <= arr[mid]) {
+                if (arr[low] <= k && k <= arr[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            else {
+                if (arr[mid] <= k && k <= arr[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
         }
         return false;
     }
