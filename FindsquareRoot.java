393c716c6806df06b095536dbaaf55b97acfbd24
tarikul <mdtarikulhassan5432@gmail.com>
2026-04-02 23:55:51 +0530

    solve q107 Finding Sqrt of a number by optimal approach; TC = O(logn)

--- FindsquareRoot.java
+++ FindsquareRoot.java
@@ -1,12 +1,27 @@
 public class FindsquareRoot {
     static int floorSqrt(int n) {
     // ---------------- brute force approach ------------ O(n)
-        int ans = 0;
-        for (int i = 1; i <= n; i++) {
-            if ((long)(i) * i <= n) {
-                ans = i;
+        // int ans = 0;
+        // for (int i = 1; i <= n; i++) {
+        //     if ((long)(i) * i <= n) {
+        //         ans = i;
+        //     } else {
+        //         break;
+        //     }
+        // }
+        // return ans;
+
+
+    // ------------------ optimal approach -------------- O(logn)
+        if (n < 2) return n;
+        int left = 1, right = n / 2, ans = 0;
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            if (mid * mid <= n) {
+                ans = (int) mid;
+                left = (int) mid + 1;
             } else {
-                break;
+                right = (int) mid - 1;
             }
         }
         return ans;
