5fb97342a9b6e23bf65e4876b4a3af7d2f3e4a1d
tarikul <mdtarikulhassan5432@gmail.com>
2026-04-02 23:40:10 +0530

    solve q106 Search Element in a Rotated Sorted Array by oprimal using binary search approach; TC = O(logn)

--- SearchinRotatedSortedArray.java
+++ SearchinRotatedSortedArray.java
@@ -1,9 +1,34 @@
 public class SearchinRotatedSortedArray {
     static int search(int[] nums, int target) {
-    // ----------------- brute force ---------------- O(n)
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                return i;
+    // ----------------- brute force by linear search approach ---------------- O(n)
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (nums[i] == target) {
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+
+    // ------------ optimal by binary search approach ---------------- O(logn)
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target)
+                return mid;
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                }
+                else {
+                    low = mid + 1;
+                }
+            }else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                }else {
+                    high = mid - 1;
+                }
             }
         }
         return -1;
