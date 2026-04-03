3678bad4bc2c2093d25c7e11f43b2a546bfe06df
tarikul <mdtarikulhassan5432@gmail.com>
2026-04-03 23:19:20 +0530

    solve q109 Peak element in Array by brute force approach; TC = O(n)

---
+++ PeakElementFinder.java
@@ -0,0 +1,17 @@
+public class PeakElementFinder {
+    static  int findPeakElement(int[] nums) {
+    // ---------------- brute force approach ---------------- O(n)
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            boolean left = (i == 0) || (nums[i] >= nums[i - 1]);
+            boolean right = (i == n - 1) || (nums[i] >= nums[i + 1]);
+            if (left && right) return i;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 20, 4, 1, 0};
+        int index = findPeakElement(nums);
+        System.out.println("Peak at index: " + index + " with value: " + nums[index]);
+    }
+}
