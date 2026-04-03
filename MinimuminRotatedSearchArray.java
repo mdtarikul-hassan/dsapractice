495a879e6f808521e11eb388e66bfdf200a7ff50
tarikul <mdtarikulhassan5432@gmail.com>
2026-04-03 23:12:44 +0530

    solve q108 Minimum in Rotated Sorted Array by optimal approach; TC = O(logn)

--- MinimuminRotatedSearchArray.java
+++ MinimuminRotatedSearchArray.java
@@ -1,11 +1,24 @@
 public class MinimuminRotatedSearchArray {
     static int findMin(int[] nums) {
     // ---------------------- brute force approach ----------------- O(n)
-        int minVal = Integer.MAX_VALUE;
-        for (int i = 0; i < nums.length; i++) {
-            minVal = Math.min(minVal, nums[i]);
+        // int minVal = Integer.MAX_VALUE;
+        // for (int i = 0; i < nums.length; i++) {
+        //     minVal = Math.min(minVal, nums[i]);
+        // }
+        // return minVal;
+
+
+    // ----------------- optimal approach ---------------- O(logn)
+        int low = 0, high = nums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > nums[high]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
         }
-        return minVal;
+        return nums[low];
     }
      public static void main(String[] args) {
         int[] nums = {4, 5, 6, 7, 0, 1, 2};
