1ddd38e2cae6ea37e4cd81939c5dec6a20408ae6
tarikul <mdtarikulhassan5432@gmail.com>
2026-04-03 23:26:10 +0530

    solve q109 Peak element in 2d Array by optimal approach; TC = O(n*logm)

--- PeakElementFinder.java
+++ PeakElementFinder.java
@@ -22,9 +22,57 @@ public class PeakElementFinder {
         }
         return low;
     }
+    // -------------- peak in 2d matrix by brute force -------------- O(n*logm)
+      static int maxElement(int[][] arr, int col) {
+          int n = arr.length;
+          int max = Integer.MIN_VALUE;
+          int index = -1;
+          for (int i = 0; i < n; i++) {
+              if (arr[i][col] > max) {
+                  max = arr[i][col];
+                  index = i;
+              }
+          }
+          return index;
+      }
+  
+      static int[] findPeakGrid(int[][] arr) {
+          int n = arr.length;
+          int m = arr[0].length;
+  
+          int low = 0;
+          int high = m - 1;
+            while (low <= high) {
+              int mid = (low + high) / 2;
+                int row = maxElement(arr, mid);
+                int left = mid - 1 >= 0 ? arr[row][mid - 1] : Integer.MIN_VALUE;
+              int right = mid + 1 < m ? arr[row][mid + 1] : Integer.MIN_VALUE;
+            if (arr[row][mid] > left && arr[row][mid] > right) {
+                return new int[]{row, mid};
+              } else if (left > arr[row][mid]) {
+                  high = mid - 1;
+              } else {
+                  low = mid + 1;
+              }
+          }
+            return new int[]{-1, -1};
+      }
     public static void main(String[] args) {
-        int[] nums = {1, 3, 20, 4, 1, 0};
-        int index = findPeakElement(nums);
-        System.out.println("Peak at index: " + index + " with value: " + nums[index]);
+        // int[] nums = {1, 3, 20, 4, 1, 0};
+        // int index = findPeakElement(nums);
+        // System.out.println("Peak at index: " + index + " with value: " + nums[index]);
+
+
+    // peak in 2d matrix
+          int[][] mat = {
+              {4, 2, 5, 1, 4, 5},
+              {2, 9, 3, 2, 3, 2},
+              {1, 7, 6, 0, 1, 3},
+              {3, 6, 2, 3, 7, 2}
+          };
+          int[] peak = findPeakGrid(mat);
+          System.out.println("The row of the peak element is " + peak[0] +
+                             " and the column of the peak element is " + peak[1]);
+
     }
 }
