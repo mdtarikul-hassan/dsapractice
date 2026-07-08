e3b4777bf3cdb374940ca092cc1c02213e5d5433
tarikul <mdtarikulhassan5432@gmail.com>
2026-07-08 17:21:07 +0530

    solve q16 alternate leetcode : 80 Remove Duplicates from Sorted Array II : T.C-> O(n)

--- RemoveDuplicateFromSorted.java
+++ RemoveDuplicateFromSorted.java
@@ -1,16 +1,37 @@
+import java.util.HashMap;
+
 public class RemoveDuplicateFromSorted {
+    // public static int removeDuplicates(int[] nums) {
+    //     if (nums.length == 0) return 0;
+
+    //     int i = 0;
+
+    //     for (int j = 1; j < nums.length; j++) {
+    //         if (nums[j] != nums[i]) {
+    //             i++;
+    //             nums[i] = nums[j];
+    //         }
+    //     }
+    //     return i + 1;
+    // }
+
+// remove duplicate and present atmost 2 of every duplicate    -> O(n)
     public static int removeDuplicates(int[] nums) {
-        if (nums.length == 0) return 0;
-
-        int i = 0;
-
-        for (int j = 1; j < nums.length; j++) {
-            if (nums[j] != nums[i]) {
-                i++;
+        if(nums.length <= 2){
+            return nums.length;
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int i=0;
+        for(int j =0; j< nums.length; j++){
+            int count = map.getOrDefault(nums[j], 0);
+            if(count < 2){
                 nums[i] = nums[j];
+                map.put(nums[i], count+1);
+                i++;
             }
         }
-        return i + 1;
+
+        return i;
     }
     public static void main(String[] args) {
         int[] nums = {0,0,1,1,1,2,2,3,3,4};
