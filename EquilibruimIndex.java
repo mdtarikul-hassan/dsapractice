d167325a6de398058f3326ccc3f9267416d38d90
tarikul <mdtarikulhassan5432@gmail.com>
2026-07-09 22:16:32 +0530

    solve q23 find Equilibruim Index by approach -> O(n)

--- EquilibruimIndex.java
+++ EquilibruimIndex.java
@@ -1,18 +1,32 @@
 public class EquilibruimIndex {
     static int equilibruimIndex(int[] nums){
     // ------------------- brute force approach ------------------ O(n2)
+        // for(int i = 0; i<nums.length; i++){
+        //     int ls = 0;
+        //     int rs = 0;
+        //     for(int j = 0; j<i; j++){
+        //         ls += nums[j];
+        //     }
+        //     for(int j = i+1; j<nums.length; j++){
+        //         rs += nums[j];
+        //     }
+        //     if(ls == rs){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+// ------------------ optimal ---------------- O(n)
+        int ls = 0, rs = 0, ts = 0;
         for(int i = 0; i<nums.length; i++){
-            int ls = 0;
-            int rs = 0;
-            for(int j = 0; j<i; j++){
-                ls += nums[j];
-            }
-            for(int j = i+1; j<nums.length; j++){
-                rs += nums[j];
-            }
+            ts += nums[i];
+        }
+        for(int i = 0; i<nums.length; i++){
+            rs = ts-ls-nums[i];
             if(ls == rs){
                 return i;
             }
+            ls += nums[i];
         }
         return -1;
     }
