5be3354c151109e3c57fe3e94d95d76273176652
tarikul <mdtarikulhassan5432@gmail.com>
2026-07-29 02:34:48 +0530

    solve q105 First and Las t Occurance in array : O(logn)

--- FirstAndLastOccurrence.java
+++ FirstAndLastOccurrence.java
@@ -2,6 +2,42 @@ import java.util.Arrays;
 import java.util.List;
 
 public class FirstAndLastOccurrence {
+    public int[] searchRange(int[] nums, int target) {
+        return new int[]{findFirst(nums, target), findLast(nums, target)};
+    }
+    private int findFirst(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int ans = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+    private int findLast(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int ans = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                ans = mid;
+                left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
     static int solve(int n, int key, List<Integer> v) {
     // ----------------------- brute force approach (Last ocurrence) -------------- O(logn)
         // int res = -1;
@@ -39,5 +75,10 @@ public class FirstAndLastOccurrence {
         List<Integer> v = Arrays.asList(3, 4, 13, 13, 13, 20, 40);
         int ans = solve(n, key, v);
         System.out.println(ans);
+
+        int[] arr = {5,7,7,8,8,10};
+        FirstAndLastOccurrence obj = new FirstAndLastOccurrence();
+        int[] res =obj.searchRange(arr, 8);
+        System.out.println("first : "+ res[0]+" and last : "+res[1]);
     }
 }
