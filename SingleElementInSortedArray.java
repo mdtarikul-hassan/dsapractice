e1c6e4c67ed33716c2c66be2571abfddceed150c
tarikul <mdtarikulhassan5432@gmail.com>
2026-07-31 02:31:07 +0530

    solve q110 Single element in a sorted array by brute force : O(n)

---
+++ SingleElementInSortedArray.java
@@ -0,0 +1,22 @@
+public class SingleElementInSortedArray {
+    static int singleNonDuplicate(int[] nums) {
+    // -------------------- brute force  -------------- O(n)
+        if(nums[0] != nums[1]){
+            return nums[0];
+        }
+        if(nums[nums.length-1] != nums[nums.length-2]){
+            return nums[nums.length-1];
+        }
+
+        for(int i = 1; i<nums.length-1; i++){
+            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,1,2,3,3,4,4,8,8};
+        System.out.println("single element is "+ singleNonDuplicate(arr));
+    }
+}
