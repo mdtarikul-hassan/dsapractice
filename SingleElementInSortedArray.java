1ee5587519c91cfc5eb4627bc82aa56c69618ef5
tarikul <mdtarikulhassan5432@gmail.com>
2026-07-31 02:57:20 +0530

    solve q110 Single element in a sorted array by optimal approach using binary search: O(logn)

--- SingleElementInSortedArray.java
+++ SingleElementInSortedArray.java
@@ -17,11 +17,35 @@ public class SingleElementInSortedArray {
 
 
     // ----------------- better approach by XOR -----------------
-        int ans = 0;
-        for(int i = 0; i<nums.length; i++){
-            ans = ans ^ nums[i];
+        // int ans = 0;
+        // for(int i = 0; i<nums.length; i++){
+        //     ans = ans ^ nums[i];
+        // }
+        // return ans;
+    
+    // ------------- optimal by binary search O(logn)    
+        if(nums.length == 1){
+            return nums[0];
         }
-        return ans;
+        if(nums[0] != nums[1]){
+            return nums[0];
+        }
+        if(nums[nums.length-1] != nums[nums.length-2]){
+            return nums[nums.length-1];
+        }
+
+        int l=1, r=nums.length-2;
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
+                return nums[mid];
+            }else if((mid %2 == 0 && nums[mid] == nums[mid-1]) || (mid %2 == 1 && nums[mid] == nums[mid+1])){
+                r = mid-1;
+            }else{
+                l = mid+1;
+            }
+        }
+        return -1;
     }
     public static void main(String[] args) {
         int[] arr = {1,1,2,3,3,4,4,8,8};
